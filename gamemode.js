25d5d95700112e873de31cc57b71b5e31e28081f
booyaalex <64170386+booyaalex@users.noreply.github.com>
2024-02-19 11:56:51 -0500

    Create gamemode.js

---
+++ gamemode.js
@@ -0,0 +1,37 @@
+//Button Functions
+
+function factoryPage() {
+  window.location.href = "factory.html";
+}
+
+function monsterPage() {
+  window.location.href = "monster.html";
+}
+
+function fishingPage() {
+  window.location.href = "fishing.html";
+}
+
+function towerOnePage() {
+  window.location.href = "towerOne.html";
+}
+
+function towerTwoPage() {
+  window.location.href = "towerTwo.html";
+}
+
+function cafePage() {
+  window.location.href = "cafe.html";
+}
+
+function cryptoPage() {
+  window.location.href = "crypto.html";
+}
+
+function goldPage() {
+  window.location.href = "gold.html";
+}
+
+function statsPage() {
+  window.location.href = "stats.html";
+}
