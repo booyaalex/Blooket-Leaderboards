9be215ab489086c7d7ac129536ab4e9db9bdfd93
booyaalex <64170386+booyaalex@users.noreply.github.com>
2024-02-19 14:37:02 -0500

    Create stats.js

---
+++ stats.js
@@ -0,0 +1,35 @@
+function winsPage() {
+  window.location.href = "winsLB.html";
+}
+
+function classicPage() {
+  window.location.href = "pointsLB.html";
+}
+
+function defeatedPage() {
+  window.location.href = "playersDefeatedLB.html";
+}
+
+function answersPage() {
+  window.location.href = "answersLB.html";
+}
+
+function brPage() {
+  window.location.href = "showdownLB.html";
+}
+
+function gamesPage() {
+  window.location.href = "gamesPlayedLB.html";
+}
+
+function blooksPage() {
+  window.location.href = "blooksLB.html";
+}
+
+function factoryPage() {
+  window.location.href = "factoryUpgradesLB.html";
+}
+
+function tokensPage() {
+  window.location.href = "tokensLB.html";
+}
