f5982dd689bb9d6ffa3091318df8252e5416f93b
booyaalex <64170386+booyaalex@users.noreply.github.com>
2024-02-19 19:09:00 -0500

    Update TD2.js

--- TD2.js
+++ TD2.js
@@ -1,69 +1,69 @@
 //Sunny Meadows
 
 function easySM() {
-  window.location.href = "TD2/easySM_LB.html";
+  window.location.href = "TD2LB/easySM_LB.html";
 }
 
 function normalSM() {
-  window.location.href = "TD2/normalSM_LB.html";
+  window.location.href = "TD2LB/normalSM_LB.html";
 }
 
 function hardSM() {
-  window.location.href = "TD2/hardSM_LB.html";
+  window.location.href = "TD2LB/hardSM_LB.html";
 }
 
 //Lost Desert
 
 function easyLD() {
-  window.location.href = "TD2/easyLD_LB.html";
+  window.location.href = "TD2LB/easyLD_LB.html";
 }
 
 function normalLD() {
-  window.location.href = "TD2/normalLD_LB.html";
+  window.location.href = "TD2LB/normalLD_LB.html";
 }
 
 function hardLD() {
-  window.location.href = "TD2/hardLD_LB.html";
+  window.location.href = "TD2LB/hardLD_LB.html";
 }
 
 //Adandoned Mine
 
 function easyAM() {
-  window.location.href = "TD2/easyAM_LB.html";
+  window.location.href = "TD2LB/easyAM_LB.html";
 }
 
 function normalAM() {
-  window.location.href = "TD2/normalAM_LB.html";
+  window.location.href = "TD2LB/normalAM_LB.html";
 }
 
 function hardAM() {
-  window.location.href = "TD2/hardAM_LB.html";
+  window.location.href = "TD2LB/hardAM_LB.html";
 }
 
 //Crossroads
 
 function easyC() {
-  window.location.href = "TD2/easyC_LB.html";
+  window.location.href = "TD2LB/easyC_LB.html";
 }
 
 function normalC() {
-  window.location.href = "TD2/normalC_LB.html";
+  window.location.href = "TD2LB/normalC_LB.html";
 }
 
 function hardC() {
-  window.location.href = "TD2/hardC_LB.html";
+  window.location.href = "TD2LB/hardC_LB.html";
 }
 
 //Fractured Factory
 
 function easyFF() {
-  window.location.href = "TD2/easyFF_LB.html";
+  window.location.href = "TD2LB/easyFF_LB.html";
 }
 
 function normalFF() {
-  window.location.href = "TD2/normalFF_LB.html";
+  window.location.href = "TD2LB/normalFF_LB.html";
 }
 
 function hardFF() {
-  window.location.href = "TD2/hardFF_LB.html";
+  window.location.href = "TD2LB/hardFF_LB.html";
 }
