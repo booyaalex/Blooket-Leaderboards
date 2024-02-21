fa927fc18399b4c9317b08b171a959bae2a654ce
booyaalex <64170386+booyaalex@users.noreply.github.com>
2024-02-21 13:27:04 -0500

    Create monster.js

---
+++ monster.js
@@ -0,0 +1,51 @@
+//Section I
+function snow() {
+  window.location.href = "MB/snow_LB";
+}
+
+function snowItem() {
+  window.location.href = "MB/snowItem_LB";
+}
+
+function snowHit() {
+  window.location.href = "MB/snowHit_LB";
+}
+
+//Section II
+function snow() {
+  window.location.href = "MB/clock_LB";
+}
+
+function snowItem() {
+  window.location.href = "MB/clockItem_LB";
+}
+
+function snowHit() {
+  window.location.href = "MB/clockHit_LB";
+}
+
+//Section III
+function snowC() {
+  window.location.href = "MB/snowC_LB";
+}
+
+function snowItemC() {
+  window.location.href = "MB/snowItemC_LB";
+}
+
+function snowHitC() {
+  window.location.href = "MB/snowHitC_LB";
+}
+
+//Section IV
+function clockC() {
+  window.location.href = "MB/clockC_LB";
+}
+
+function clockItemC() {
+  window.location.href = "MB/clockItemC_LB";
+}
+
+function clockHitC() {
+  window.location.href = "MB/clockHitC_LB";
+}
