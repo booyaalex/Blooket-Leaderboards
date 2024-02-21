0ab43531db57b32964f94b8c9c608da7aa279c13
booyaalex <64170386+booyaalex@users.noreply.github.com>
2024-02-20 19:00:33 -0500

    Create towerOne.js

---
+++ towerOne.js
@@ -0,0 +1,75 @@
+//Section 1
+function sunny() {
+  window.location.href = "TD/SM_LB.html";
+}
+
+function lost() {
+  window.location.href = "TD/LD_LB.html";
+}
+
+function mine() {
+  window.location.href = "TD/AM_LB.html";
+}
+
+//Section 2
+function chick() {
+  window.location.href = "TD/Chick_LB.html";
+}
+
+function squirrel() {
+  window.location.href = "TD/Squirrel_LB.html";
+}
+
+function owl() {
+  window.location.href = "TD/Owl_LB.html";
+}
+
+function pig() {
+  window.location.href = "TD/Pig_LB.html";
+}
+
+function fish() {
+  window.location.href = "TD/Fish_LB.html";
+}
+
+function elf() {
+  window.location.href = "TD/Elf_LB.html";
+}
+
+function dragon() {
+  window.location.href = "TD/Dragon_LB.html";
+}
+
+function wizzard() {
+  window.location.href = "TD/Wizzard_LB.html";
+}
+
+function unicorn() {
+  window.location.href = "TD/Unicorn_LB.html";
+}
+
+//Section 3
+function sunnySpeed() {
+  window.location.href = "TD/SM_Speed_LB.html";
+}
+
+function lostSpeed() {
+  window.location.href = "TD/LD_Speed_LB.html";
+}
+
+function mineSpeed() {
+  window.location.href = "TD/AM_Speed_LB.html";
+}
+
+//Section 4
+function mostDamage() {
+  window.location.href = "TD/mostDamageLB.html";
+}
+
+function totalDamage() {
+  window.location.href = "TD/totalDamageLB.html";
+}
+
+function totalRounds() {
+  window.location.href = "TD/totalRoundsLB.html";
+}
