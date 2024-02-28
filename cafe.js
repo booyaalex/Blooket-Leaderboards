dd6360e9a05a6d67823774b8d8471b3352691968
booyaalex <64170386+booyaalex@users.noreply.github.com>
2024-02-28 10:16:35 -0500

    Create cafe.js

---
+++ cafe.js
@@ -0,0 +1,39 @@
+//Highscores
+
+function cafeDay() {
+  window.location.href = "cafe/highDay_LB.html";
+}
+
+function totalFood() {
+  window.location.href = "cafe/totalFood_LB.html";
+}
+
+function totalCash() {
+  window.location.href = "cafe/totalCash_LB.html";
+}
+
+//Speedruns
+
+function toast99() {
+  window.location.href = "cafe/99Toast_LB.html";
+}
+
+function maxUpgrade() {
+  window.location.href = "cafe/maxUpgrade_LB.html";
+}
+
+function day10() {
+  window.location.href = "cafe/day10_LB.html";
+}
+
+function day50() {
+  window.location.href = "cafe/day50_LB.html";
+}
+
+function allFood() {
+  window.location.href = "cafe/allFood_LB.html";
+}
+
+function maxFood() {
+  window.location.href = "cafe/maxFood_LB.html";
+}
