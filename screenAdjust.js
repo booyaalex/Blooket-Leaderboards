01b0823a28f66ba95eead3ae27318d538de2e584
booyaalex <64170386+booyaalex@users.noreply.github.com>
2024-02-29 07:53:17 -0500

    Create screenAdjust.js

---
+++ screenAdjust.js
@@ -0,0 +1,10 @@
+let width = screen.availWidth;
+let screenMode = 0;
+//0 Is Comp; 1 is Phone; 2 is Big Comp
+
+if (width <= 644) {
+  let screenMode = 1;
+  document.getElementById("navBar").innerHTML = "";
+}
+console.log(width);
+console.log(screenMode);
