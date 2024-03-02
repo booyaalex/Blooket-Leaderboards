18f13133a99db2bfd415f70f2b0ac9fbaaf252b6
booyaalex <64170386+booyaalex@users.noreply.github.com>
2024-03-02 14:53:10 -0500

    Create pop.js

---
+++ Event/pop.js
@@ -0,0 +1,24 @@
+//Nav Functions
+function home() {
+  window.location.href = "https://booyaalex.github.io/Blooket-Leaderboards/index.html";
+}
+
+function LB() {
+  window.location.href = "https://booyaalex.github.io/Blooket-Leaderboards/Event/pop.html";
+}
+
+function dance() {
+  window.location.href = "https://booyaalex.github.io/Blooket-Leaderboards/Event/pop/funni.html";
+}
+
+function place() {
+  window.location.href = "https://booyaalex.github.io/Blooket-Leaderboards/Event/pop/place.html";
+}
+
+function ingredients() {
+  window.location.href = "https://booyaalex.github.io/Blooket-Leaderboards/Event/pop/ingredients.html";
+}
+
+function potions() {
+  window.location.href = "https://booyaalex.github.io/Blooket-Leaderboards/Event/pop/potions.html";
+}
