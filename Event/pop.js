6e15c9f3bcbb13889c4aca1bd810c6df210e7fc9
booyaalex <64170386+booyaalex@users.noreply.github.com>
2024-03-14 17:08:51 -0400

    New Link

--- Event/pop.js
+++ Event/pop.js
@@ -1,24 +1,24 @@
 //Nav Functions
 function home() {
-  window.location.href = "https://booyaalex.github.io/Blooket-Leaderboards/index.html";
+  window.location.href = "https://blooket.games/index.html";
 }
 
 function LB() {
-  window.location.href = "https://booyaalex.github.io/Blooket-Leaderboards/Event/pop.html";
+  window.location.href = "https://blooket.games/Event/pop.html";
 }
 
 function dance() {
-  window.location.href = "https://booyaalex.github.io/Blooket-Leaderboards/Event/pop/funni.html";
+  window.location.href = "https://blooket.games/Event/pop/funni.html";
 }
 
 function place() {
-  window.location.href = "https://booyaalex.github.io/Blooket-Leaderboards/Event/pop/place.html";
+  window.location.href = "https://blooket.games/Event/pop/place.html";
 }
 
 function workshop() {
-  window.location.href = "https://booyaalex.github.io/Blooket-Leaderboards/Event/pop/ingredients.html";
+  window.location.href = "https://blooket.games/Event/pop/ingredients.html";
 }
 
 function potions() {
-  window.location.href = "https://booyaalex.github.io/Blooket-Leaderboards/Event/pop/potions.html";
+  window.location.href = "https://blooket.games/Event/pop/potions.html";
 }
