eaf63010507b6bd12a5e9b4372cd112d923e59ef
AJIR <155471185+JPKEEFEAJ@users.noreply.github.com>
2024-04-02 12:27:57 -0400

    Did some stuff alright - first CK LB page done - Started work to have less js files - started making the links a bit cleaner

---
+++ windowLocation.js
@@ -0,0 +1,17 @@
+/*Crazy Kingdom*/
+
+function maxMaterials() {
+    window.location.href = "kingdom/maxMaterials_LB.html";
+}
+
+function maxPeople() {
+    window.location.href = "kingdom/maxMaterials_LB.html";
+}
+
+function maxHappiness() {
+    window.location.href = "kingdom/maxMaterials_LB.html";
+}
+
+function maxGold() {
+    window.location.href = "kingdom/maxMaterials_LB.html";
+}
