bf85723510baa1e263ad0448a3d6f65f5e7a9724
AJIR <155471185+JPKEEFEAJ@users.noreply.github.com>
2024-04-02 12:51:06 -0400

    Fixed CK js

--- windowLocation.js
+++ windowLocation.js
@@ -1,17 +1,17 @@
 /*Crazy Kingdom*/
 
-function maxMaterials() {
+function maxMaterial() {
     window.location.href = "kingdom/maxMaterials_LB.html";
 }
 
 function maxPeople() {
-    window.location.href = "kingdom/maxMaterials_LB.html";
+    window.location.href = "kingdom/maxPeople_LB.html";
 }
 
 function maxHappiness() {
-    window.location.href = "kingdom/maxMaterials_LB.html";
+    window.location.href = "kingdom/maxHappiness_LB.html";
 }
 
 function maxGold() {
-    window.location.href = "kingdom/maxMaterials_LB.html";
+    window.location.href = "kingdom/maxGold_LB.html";
 }
