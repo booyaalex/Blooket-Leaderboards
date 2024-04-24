a383853b864a2139bda7b3eb09e70bc053730764
booyaalex <64170386+booyaalex@users.noreply.github.com>
2024-04-24 10:37:39 -0400

    Should Work

--- submitStats.js
+++ submitStats.js
@@ -9,64 +9,66 @@ const firebaseConfig = {
 };
 firebase.initializeApp(firebaseConfig);
 
+function getDemStats() {
+    let OI = [];
+    OI = [JSON.parse(document.getElementById("epic_stats").value)];
+    console.log(OI);
 
-let OI = [];
-OI = [{"_id":"609eb290e9df5c001b1fc1c6","isUnsubscribed":false,"numUnlocks":112,"wins":242,"topFives":344,"totalPoints":0,"totalBlooks":0,"playersDefeated":2298,"avgPoints":0,"tens":0,"fifties":0,"classicPoints":256,"racingProgress":1365,"gamesPlayed":463,"correctAnswers":38532,"racingCorrects":0,"racesWon":0,"totalCash":42282223694374,"upgrades":5051,"showdownWins":79,"totalCandy":0,"totalGold":68977744862824460,"totalBrawlXp":86703,"totalToys":19496813869,"totalCrypto":18663772078773500,"totalFishWeight":164193940,"boxesOpened":1089,"gameHistory":[{"blookUsed":"1#9#0#2#4$8#0#0#0#0#0#0$0","date":"2024-03-14 17:46","name":"JohnVonAmerika","place":1,"weight":6854089},{"blookUsed":"1#9#0#2#4$8#0#0#0#0#0#0$0","date":"2024-03-14 17:51","name":"johann","numBlooks":20,"place":1},{"blookUsed":"Lovely Peacock","date":"2024-03-15 17:01","name":"You","place":21,"xp":1001},{"blookUsed":"Lovely Peacock","date":"2024-03-15 18:41","name":"You","place":50,"xp":94},{"blookUsed":"Lovely Peacock","date":"2024-03-15 18:48","name":"You","place":26,"xp":803},{"blookUsed":"White Rabbit","cash":1862442871,"date":"2024-04-17 22:25","name":"Leap!!!","place":1},{"blookUsed":"1#9#0#2#4$8#0#0#0#0#0#0$0","date":"2024-04-20 20:22","name":"leap","place":1,"weight":6707766},{"blookUsed":"Giraffe","date":"2024-04-20 20:31","name":"is trash","place":6,"weight":8414705},{"blookUsed":"Two of Spades","date":"2024-04-20 20:42","name":"leap","place":3,"weight":5415877},{"blookUsed":"1#9#0#2#4$8#0#0#0#0#0#0$0","date":"2024-04-22 18:46","name":"nnahoj","place":1,"weight":4786488}],"favorites":["60b4fa16c711550023b3718d","6079c2c87b2710002146e21c","620aee67ff5354c50b4f738c","62042b2ab84406a2c67eabf3","60a292560a2a0c001b378c57","615cc25a0f99d400667eed22","600b1491d42a140004d5215a","5fc91a9b4ea2e200046bd49a","6181dcd1ea6937e8776689f9","60c9c3a375561f001d1a88e8","63014069d45bf2cd8328a878","5df02ca892af970017029a0d","61f2bc01ba997d8a066f93f3","624a214d6394a077c219ce87","639c6c3653a63c317a5cd284","61547ea3a32d0c00366dfe77","6223989d79fc0a1d599614aa"],"tokens":1706,"totalTokens":31706,"tokensAvailable":420,"news":false,"plan":"Starter","stagesCleared":388,"towerClears":1,"towerSaves":[null,"653174a7ed759fceab714eee","6578902b189bcf582e32d166"],"cafeCash":145864678,"foodServed":6019,"cafeSaves":["616ee97200ac240037aa35d3","61b3db1638572c028f7d9507","65672ec54ecd856aee1356cc"],"defenseRounds":1213,"defenseDmg":441583776,"defenseSaves":["627e5715a807e29355b57936","63b360a3a9e5dc594afffdcf","6589e9d0552137d1cf323ce0"],"guestsAnswered":1388,"games":["61ab7a8197ca2a974de17e59","62181d34ddc696ef2ca16ba9","62213503b8223411c7272f65","62295086b3532b3f2e101ed2","622aa0659bfd81a192dff1e2","622bc277e8df0d29805094e5","62332b66fbfe1f126bf0d69d","62332bb9fbfe1f126bf0da7f","623ba3d7d99ba177d824df67","625f3b7c0bc63ccc2642ed1c","628ffeb0d715a00b7f92ae65","6344985c8c507ec1078af816","635840b7c3126cf9a25063b1","636aee84fee9fad93d618cda","63780f3103e9b02b397034bb","63911d3b0adbdea232724595","63a06f1d34b70a84549e9016","63c9e179b54821df0463ad5a","63e2656bca604abdffa7129c","63f4ce4a6ad43cff5f27d710","63f68d0d51007d77cd49b9ff","65120303ba0bb577d186f8fe","651c57fe9be42e948dc60390","652959e0b428d13a8e116f1e","65672e8ebafff5b1d5075416","65c6644d984098b3881af999","65c66481d4893f3171a59c07","661dab7e38d5b9dc50090282"],"histories":["60b6708aa2676e001b412d55","60b6724ce9922500212b3027","6157064a67b0aa00368c272c","61572463bca2b200669c9e60","6157259804f1fd0037c32db2","615d9d62a43df5002ac487b4","61697ac1f0792d002a83b28c","61697c93397970007a61fa3f","616ec21334b9ed0030ea011f","61716336dd681c00371df3bc","61794dfebf13ae80c0e682fb","6182da4b9b86022ce6f2ce00","6186dd0e710e578bad52ac2a","6189aad921a1b4f4d475685c","618b037fc6107e3c72f3e3a2","618b0564c6107e3c72f3e859","618b06e4887ef2a6129b6f3e","618d20dcd82178e3a7a59cd5","618d22d210f32c6e9d1bc5d9","6192820fb9b346908adad0d9","61981f941c5a79940b7f58ca","61982014b388b68d5956f7a1","61aa221d445d363f57488ea4","61aaa983b8b0c7fdb2c57ba2","61aaaaf4b8b0c7fdb2c57e9a","61b8a4922340e70746c94aa7","61bc9c364b2f603ada39acf4","61c0f4868767d14c381cf8da","61c265ff9b22ed636901846b","61f83d115f6cba3811843f74","61ff0091edfad0878f502ebf","622b6215e8df0d298049cd36","62332fe3fbfe1f126bf111f8","623e1592aa8e17e3f1552133","623e1627ce758ef698fcc0ce","623e1694aa8e17e3f1553098","625adf24af2f64b58564c665","625ae034af2f64b58564c7bd","625ae212e2e55575709a1a85","625ae25be2e55575709a1aff","625f3d2857f25b68fc7af966","625f3deb57f25b68fc7afb3e","625f3ebb0bc63ccc2642f53b","625f3f7257f25b68fc7aff1f","627810a4761c123d3492056f","6278128f21e2151edc437d73","627f918384667318e9aec2f0","6290118cc0dc6983bae567c0","62901529d715a00b7f92d1f5","6296ab4aa2147c4745ba5d30","6296adb3a2147c4745ba60ae","6296b062a2147c4745ba64a8","6298dade8fbeab46bc8d3355","6298f3e68fbeab46bc8dd8fd","62e8339674e226288700979e","62e8366e8699c46e4422bd77","62e83a5674e22628870099c3","62fbbe65565b62c1f6cebd42","62fbbeec961a1e73ac1972b9","62fc36fc63829de4af429c60","6307e181b5a03112b738e916","6307e27538805151106bd296","6307e30e53bb827f18d37caa","630a9ab7fc96b8147c1322b2","630ea42c124f2ca5a08cc0f2","631694432f1e34d1dbaf63f7","6319e656c3d95b5b35735df2","63337f56dc94f29d0aca28db","6334d4538aa179c20fb6bd54","63362d2e7fd114a2f50f1d55","63362f197fd114a2f50f202c","634b1a312760f3a4dabc7973","634b1b1a2760f3a4dabc7a6d","634b1babcfc207bd4b0f2242","634b1d90cfc207bd4b0f2452","634b1f87cfc207bd4b0f2675","634b2315cfc207bd4b0f2a15","634b25d12760f3a4dabc859b","634b288dcfc207bd4b0f2fea","634b298e2760f3a4dabc897e","634d5ca9e43c0a6dfc3a468b","634d5e11e43c0a6dfc3a489c","634d5ed1101b205e1620569c","634f3b6fbd9a3ca8fd80ef6b","634f3e2bc5d6256ae83d0f21","634f412fbfeac41e4ab9431f","635146c8ef8d1773818a3594","6351b199de301cd6e0d8ed1e","6351b50ade301cd6e0d8f19d","635418e3f4841568cb521f37","63542058f4841568cb522a25","635421583cf06ae0d4fc9228","63645516afc50cdf2348a8b1","636459ab4278b835d3e7bfef","63645c85a8734168a2408c13","63645e672cb2c773e95e204a","637646dbc79c8853dbe88b35","637648c91fa86b15a4948b5b","63764accd37f7214b25875ea","6377935ea012956b8946b5bf","637ad90090194f07ad08fe6e","63911e1d87d5b54596abe332","639ca46d1d4fa5130ea36969","63a3134c740c200dce8d1d83","63a3163f5b8415c3748e3627","63bc1eb59f1ffa74971530d1","63c2de6b16a95239f9d8ff29","63c9e24cf90de57a694abbb0","63d3ddd0ad6a1ded4f2b2f2d","63d3e0523468eca7ee42767e","63f8d0ba850963a5f68fa5cb","64022d0043027edc9fcb1c5d","6405efd8856b9d39f2296b80","6441d56ec2a9fc3786ffcefe","65295cd9f8dd69705f2bbd27","6532c548d4f99463d2c5f09c"],"homeworks":["6352fad96b754ec92a883159","6393856b67a8d4a43ee53688","63b3608ba9e5dc594afffdc0","66203bd085a2e1343cab314c"],"questCount":0,"numRefers":0,"name":"leap541","blook":"Macaw","dateCreated":"2021-05-14T17:25:36.089Z","lastTokenDay":"2024-04-22T00:00:00Z","modalMessage":"","quests":{},"unlocks":{"Alice":1,"Alien":2,"Amber":1,"Angry Bot":16,"Baby Shark":1,"Blobfish":1,"Brainy Bot":5,"Breakfast Combo":1,"Brontosaurus":1,"Buccaneer":4,"Buddy Bot":5,"Candy Corn":7,"Caramel Apple":8,"Caterpillar":1,"Cereal":1,"Cheshire Cat":1,"Chick Chicken":1,"Clownfish":2,"Crab":1,"Deckhand":4,"Dingo":5,"Dink":1,"Dino Egg":1,"Dino Fossil":1,"Donk":2,"Dormouse":1,"Dragon":1,"Drink Me":1,"Earth":2,"Eat Me":1,"Echidna":6,"Elephant":1,"Elf":2,"Fairy":1,"Flamingo":1,"Frankenstein":15,"French Toast":1,"Frog":1,"Gingerbread House":4,"Gingerbread Man":3,"Happy Bot":7,"Holiday Gift":6,"Holiday Wreath":7,"Hot Chocolate":6,"Ice Bat":1,"Ice Bug":1,"Ice Elemental":1,"Jellyfish":1,"Jester":1,"Joey":3,"Jolly Pirate":1,"Kangaroo":2,"King":1,"King of Hearts":1,"Koala":5,"Kookaburra":4,"Lemur":1,"Lil Bot":13,"Lovely Bot":10,"Lovely Frog":1,"Lovely Peacock":1,"Lovely Planet":1,"Lucky Frog":1,"Lucky Hamster":1,"Mad Hatter":1,"Meteor":1,"Milk":1,"Mummy":3,"Narwhal":1,"Octopus":1,"Old Boot":1,"Orange Juice":1,"Pancakes":1,"Panda":1,"Peacock":1,"Pirate Ship":2,"Pizza":1,"Planet":2,"Pufferfish":1,"Pumpkin":10,"Queen":1,"Queen of Hearts":1,"Rock Monster":1,"Seagull":2,"Slime Monster":1,"Sloth":1,"Snow Globe":6,"Snowman":2,"Spaceship":1,"Stars":2,"Stegosaurus":1,"Stocking":1,"Swamp Monster":12,"Swashbuckler":2,"Tenrec":1,"Toast":1,"Treasure Map":1,"Two of Spades":1,"Tyrannosaurus Rex":1,"UFO":1,"Unicorn":1,"Vampire":11,"Velociraptor":2,"Waffle":1,"Watson":5,"Werewolf":2,"White Rabbit":1,"Witch":1,"Wizard":1,"Yogurt":1,"Zebra":1,"Zombie":6},"blookUsage":{"Alien":2,"Anaconda":2,"Angry Bot":3,"Arctic Fox":2,"Arctic Hare":3,"Astronaut":2,"Baby Penguin":2,"Baby Shark":1,"Bear":2,"Beaver":2,"Brainy Bot":6,"Brontosaurus":1,"Buddy Bot":1,"Capuchin":3,"Cat":7,"Caterpillar":2,"Cheshire Cat":3,"Chick":2,"Chick Chicken":4,"Chicken":2,"Clownfish":1,"Cockatoo":3,"Crab":1,"Deer":2,"Dog":3,"Dormouse":1,"Dragon":7,"Drink Me":1,"Duck":4,"Elf":1,"Fairy":6,"Fox":1,"French Toast":1,"Gingerbread House":2,"Giraffe":1,"Goat":3,"Goldfish":2,"Happy Bot":2,"Hedgehog":3,"Horse":1,"Ice Bat":1,"Jaguar":2,"Jester":2,"King":14,"King of Hearts":12,"Kitten":1,"Lemur":2,"Lovely Planet":1,"Lucky Frog":3,"Macaw":4,"Mad Hatter":4,"Master Elf":2,"Mega Bot":8,"Meteor":1,"Moose":3,"Mummy":2,"Octopus":1,"Orangutan":4,"Owl":1,"Panda":1,"Panther":4,"Parrot":1,"Peacock":1,"Penguin":2,"Phantom King":1,"Pig":2,"Pizza":58,"Planet":1,"Polar Bear":2,"Queen":7,"Rabbit":2,"Raccoon":2,"Rhino":1,"Rock Monster":1,"Santa Claus":1,"Seal":1,"Sheep":2,"Slime Monster":5,"Snowman":4,"Snowy Owl":6,"Spaceship":3,"Squirrel":7,"Tiger":2,"Two of Spades":1,"UFO":1,"Unicorn":7,"Vampire":2,"Walrus":2,"Watson":2,"Werewolf":2,"White Rabbit":5,"Witch":4,"Wizard":1},"folders":null,"favoriteFolders":null,"xp":3315,"xpAvailable":253,"customBlooks":["1#87#0#0#12$0#82#0#1#1#0#0$0","1#12#0#16#15$8#0#0#1#0#0#0$0","1#60#0#0#4$8#53#41#0#0#0#0$0","1#9#0#2#4$8#0#0#0#0#0#0$0","1#11#0#0#4$8#12#21#6#1#0#0$0"],"banner":"footballField","title":"competitor","banners":["chalkboard","slime","toasterPastry","bookshelf","theater","sushi","workbench","spooky","fallingBlocks","harvest","winterDrive","outerSpace","techChip","endRainbow","sunset","pirateMap","spookyCat","winterSweater","footballField","scienceClass"],"titles":["earlyBird","lineLeader","smartyPants","studious","thoughtful","pumpkinSpice","thankful","musician","friend","competitor","creative","pirate","busy","adventurer"],"blookParts":{"cheeks":[1,2,3,4,5,6,7],"clothing":[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,45,44,36,46,37,38,39,40,41,42,43,53,54,55,56,57,58,59,60,61,62,63,64,83,65,66,67,68,69,70,71,87,72,73,74,75,76,77],"eyebrows":[1,2,3,4,5,6,7,8,9],"eyes":[1,2,3,4,5,6,7,8,9,10,11,12,13,14],"glasses":[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22],"hair":[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,19,20,21,22,23,24,25],"hat":[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,40,38,37,39,31,44,32,33,34,35,36,48,49,50,51,52,53,54,55,56,79,57,58,59,60,61,62,63,64,82,65,66,67,68,69,70,71],"item":[1,2,3,4,5,9,6,7,8,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54],"mouth":[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21],"nose":[1,2,3,4,5,6,7]}}];
-  console.log(OI);
+    console.log(OI[0]._id);
 
-  console.log(OI[0]._id);
+    const displayName = document.getElementById("display_name").value;
+    console.log(displayName);
+    let temp = OI[0].blook;
+    const displayBlook = temp.toLowerCase();
 
-  const displayName = "Leap";
-  let temp = OI[0].blook;
-  const displayBlook = temp.toLowerCase();
+    const totalWins = OI[0].wins;
+    const totalAnswers = OI[0].correctAnswers;
+    const fishingWeight = OI[0].totalFishWeight;
+    const totalPoints = OI[0].totalPoints;
+    const showdownWins = OI[0].showdownWins;
+    const factoryUpgrades = OI[0].upgrades;
+    const boxesOpened = OI[0].boxesOpened;
+    const playersDefeated = OI[0].playersDefeated;
+    const gamesPlayed = OI[0].gamesPlayed;
+    const totalTokens = OI[0].totalTokens;
+    const dateCreated = OI[0].dateCreated;
 
-  const totalWins = OI[0].wins;
-  const totalAnswers = OI[0].correctAnswers;
-  const fishingWeight = OI[0].totalFishWeight;
-  const totalPoints = OI[0].totalPoints;
-  const showdownWins = OI[0].showdownWins;
-  const factoryUpgrades = OI[0].upgrades;
-  const boxesOpened = OI[0].boxesOpened;
-  const playersDefeated = OI[0].playersDefeated;
-  const gamesPlayed = OI[0].gamesPlayed;
-  const totalTokens = OI[0].totalTokens;
-  const dateCreated = OI[0].dateCreated;
+    const test = OI[0].unlocks;
+    console.log(test);
+    const totalBlooks = Object.keys(test).length;
+    
+    const contactInfo = document.getElementById("contact_info").value;
 
-  const test = OI[0].unlocks;
-  console.log(test);
-  const totalBlooks = Object.keys(test).length;
-  
-  const contactInfo = "test";
-
-  firebase.database().ref("/").child(displayName).update({
-    "blook" : displayBlook,
-    "name" : displayName,
-    "username" : OI[0].name,
-    "test" : [
-        "totalWins",
-        totalWins,
-        "totalAnswers",
-        totalAnswers,
-        "totalBlooks",
-        totalBlooks,
-        "fishingWeight",
-        fishingWeight,
-        "totalPoints",
-        totalPoints,
-        "showdownWins",
-        showdownWins,
-        "factoryUpgrades",
-        factoryUpgrades,
-        "boxesOpened",
-        boxesOpened,
-        "playersDefeated",
-        playersDefeated,
-        "gamesPlayed",
-        gamesPlayed,
-        "totalTokens",
-        totalTokens,
-        "dateCreated",
-        dateCreated
-    ],
-    "contact" : contactInfo
-  });
+    firebase.database().ref("/").child(displayName).update({
+        "blook" : displayBlook,
+        "name" : displayName,
+        "username" : OI[0].name,
+        "test" : [
+            "totalWins",
+            totalWins,
+            "totalAnswers",
+            totalAnswers,
+            "totalBlooks",
+            totalBlooks,
+            "fishingWeight",
+            fishingWeight,
+            "totalPoints",
+            totalPoints,
+            "showdownWins",
+            showdownWins,
+            "factoryUpgrades",
+            factoryUpgrades,
+            "boxesOpened",
+            boxesOpened,
+            "playersDefeated",
+            playersDefeated,
+            "gamesPlayed",
+            gamesPlayed,
+            "totalTokens",
+            totalTokens,
+            "dateCreated",
+            dateCreated
+        ],
+        "contact" : contactInfo
+    });
+}
