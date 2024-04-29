caa1d0decb6b7a868320295d9a8cb6352c250612
booyaalex <64170386+booyaalex@users.noreply.github.com>
2024-04-28 20:25:07 -0400

    Combining Some JS Files

--- windowLocation.js
+++ windowLocation.js
@@ -1,24 +1,71 @@
+/*Cafe*/
+function cafeDay() {
+    window.location.href = "cafe/highDay_LB";
+}
+function totalFood() {
+    window.location.href = "cafe/totalFood_LB";
+}
+function totalCash() {
+    window.location.href = "cafe/totalCash_LB";
+}
+function toast99() {
+    window.location.href = "cafe/99Toast_LB";
+}
+function maxUpgrade() {
+    window.location.href = "cafe/maxUpgrade_LB";
+}
+function day10() {
+    window.location.href = "cafe/day10_LB";
+}
+function day50() {
+    window.location.href = "cafe/day50_LB";
+}
+function allFood() {
+    window.location.href = "cafe/allFood_LB";
+}
+function maxFood() {
+    window.location.href = "cafe/maxFood_LB";
+}
+
+
 /*Crazy Kingdom*/
 
 function maxMaterial() {
     window.location.href = "kingdom/maxMaterials_LB";
 }
-
 function maxPeople() {
     window.location.href = "kingdom/maxPeople_LB";
 }
-
 function maxHappiness() {
     window.location.href = "kingdom/maxHappiness_LB";
 }
-
 function maxGold() {
     window.location.href = "kingdom/maxGold_LB";
 }
-
 function mostPeople() {
     window.location.href = "kingdom/mostPeople_LB";
 }
 function fastestDeath() {
     window.location.href = "kingdom/fastestDeath_LB";
+}
+
+
+/*Factory*/
+function mostCash1() {
+    window.location.href = "factory/mostCash1_LB";
+}
+function mostCash7() {
+    window.location.href = "factory/mostCash7_LB";
+}
+function mostCash60() {
+    window.location.href = "factory/mostCash60_LB";
+}
+function cash1M() {
+    window.location.href = "factory/cash1M_LB";
+}
+function cash1B() {
+    window.location.href = "factory/cash1B_LB";
+}
+function cash1T() {
+    window.location.href = "factory/cash1T_LB";
 }
