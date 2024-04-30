becbd9cfb47961fb118750f8029612befae7db93
booyaalex <64170386+booyaalex@users.noreply.github.com>
2024-04-30 13:56:22 -0400

    hope this work

--- searchBar.js
+++ searchBar.js
@@ -1,4 +1,5 @@
 function search() {
     let searchTerm = document.getElementById("searchBar_Input_Bar").value;
     window.location.href = "#" + searchTerm;
+    document.getElementById(searchTerm).style.backgroundColor = "#9a49aa";
 }
