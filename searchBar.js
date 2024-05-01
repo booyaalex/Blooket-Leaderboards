807ae95cf96f210976cb403d2df45ef668e041bc
booyaalex <64170386+booyaalex@users.noreply.github.com>
2024-04-30 21:21:34 -0400

    should be fixed

--- searchBar.js
+++ searchBar.js
@@ -1,10 +1,9 @@
 function search() {
     const tr = document.getElementsByTagName("tr");
-    console.log(tr);
     for (let i = 0; i < tr.length; i++) {
-        tr[i].backgroundColor = "#40115f";
+        tr[i].style.backgroundColor = "#40115f";
     }
     let searchTerm = document.getElementById("searchBar_Input_Bar").value;
-        window.location.href = "#" + searchTerm;
-        document.getElementById(searchTerm).style.backgroundColor = "#9a49aa";
+    window.location.href = "#" + searchTerm;
+    document.getElementById(searchTerm).style.backgroundColor = "#9a49aa";
 }
