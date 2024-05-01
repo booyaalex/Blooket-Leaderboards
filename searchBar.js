ad3eef36610a6bc42d02ed4cc786ec0ee882cc47
booyaalex <64170386+booyaalex@users.noreply.github.com>
2024-04-30 21:12:15 -0400

    seeing if it only shows one

--- searchBar.js
+++ searchBar.js
@@ -1,5 +1,9 @@
 function search() {
+    const tr = document.getElementsByTagName("tr");
+    for (let i = 0; i < tr.length; i++) {
+        tr[i].backgroundColor = "#40115f";
+    }
     let searchTerm = document.getElementById("searchBar_Input_Bar").value;
-    window.location.href = "#" + searchTerm;
-    document.getElementById(searchTerm).style.backgroundColor = "#9a49aa";
+        window.location.href = "#" + searchTerm;
+        document.getElementById(searchTerm).style.backgroundColor = "#9a49aa";
 }
