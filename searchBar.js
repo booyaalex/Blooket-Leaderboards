026e39bb9a6ee5285b94a12367dc8d7db670053d
booyaalex <64170386+booyaalex@users.noreply.github.com>
2024-05-01 07:39:26 -0400

    this may work, who knows

--- searchBar.js
+++ searchBar.js
@@ -7,3 +7,19 @@ function search() {
     window.location.href = "#" + searchTerm;
     document.getElementById(searchTerm).style.backgroundColor = "#9a49aa";
 }
+
+$(function() {
+    let searchTerm = document.getElementById("searchBar_Input_Bar").value;
+    $("#searchBar_Button").on("click", (e) => {
+      let yBlockOffset = $("#" + searchTerm).offset().top;
+      let headerHeight = $("#navBar").height();
+      let margins = parseInt($("#" + searchTerm).css('margin-top'));
+      let totalScroll = yBlockOffset - headerHeight - margins;
+  
+      $('html, body').animate({
+          scrollTop: totalScroll
+      }, 1000);
+  
+      e.preventDefault();
+    });
+   });
