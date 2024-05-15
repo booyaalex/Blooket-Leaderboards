7c168d734d12176386be8b94b378a65c372b86af
AJIR <155471185+JPKEEFEAJ@users.noreply.github.com>
2024-05-15 10:30:27 -0400

    should be done yippee

--- Event/popGuilds/makeDemLBs.js
+++ Event/popGuilds/makeDemLBs.js
@@ -13,50 +13,45 @@ async function makeLB() {
         let name = document.createElement("div");
         name.classList.add("boxSec");
 
-        let nameSpan = document.createElement("p");
-        nameSpan.className = "boxTxt";
+        let nameText = document.createElement("p");
+        nameText.className = "boxTxt";
         let textnode1 = document.createTextNode(JSON[GUILD].members[i].name);
-        nameSpan.appendChild(textnode1);
+        nameText.appendChild(textnode1);
         
-        name.appendChild(nameSpan);
+        name.appendChild(nameText);
 
 
         //User
-        let user = document.createElement("td");
-        user.className = "tdUser";
+        let user = document.createElement("div");
+        user.classList.add("boxSec");
+        user.classList.add("user");
 
-        let userSpan = document.createElement("span");
-        userSpan.className = "boxTextUser";
-        userSpan.style.float = "right";
+        let userText = document.createElement("p");
+        userText.classList.add("boxTxt");
+        userText.classList.add("user");
 
         let textnode2 = document.createTextNode("Blooket: " + JSON[GUILD].members[i].blooketName);
-        userSpan.appendChild(textnode2);
+        userText.appendChild(textnode2);
 
-        user.appendChild(userSpan);
+        user.appendChild(userText);
 
 
         //Gold
-        let gold = document.createElement("td");
-        gold.className = "tdGold";
+        let gold = document.createElement("div");
+        gold.classList.add("boxSec");
+        gold.classList.add("gold");
 
-        let DIV = document.createElement("div");
-        DIV.className = "spacingBoxRight";
-
-        let goldSpan = document.createElement("span");
-        goldSpan.className = "boxText";
+        let goldText = document.createElement("p");
+        gold.classList.add("boxTxt");
         let temp = JSON[GUILD].members[i].gold;
         let textnode3 = document.createTextNode(temp.toLocaleString());
-        goldSpan.appendChild(textnode3);
-        DIV.appendChild(goldSpan);
+        goldText.appendChild(textnode3);
+        gold.appendChild(goldText);
 
         let IMG = document.createElement("img");
         IMG.src = "https://i.ibb.co/MfTLP2t/gold.png";
         IMG.alt = "Gold Icon";
-        IMG.style.width = "25px";
-        IMG.style.height = "25px";
-        DIV.appendChild(IMG);
-
-        gold.appendChild(DIV);
+        gold.appendChild(IMG);
 
 
         BOX.appendChild(name);
