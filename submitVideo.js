3a57b1795024e6be583796ce29dcf13d146330bd
booyaalex <64170386+booyaalex@users.noreply.github.com>
2024-06-20 18:10:45 -0400

    Should be able to make a way to verify runs much easier.

--- submitVideo.js
+++ submitVideo.js
@@ -3,70 +3,98 @@ const idArray = [];
 let OI = [];
 
 import {
-    initializeApp
+  initializeApp
 } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
 import {
-    getStorage,
-    ref,
-    uploadBytes,
+  getStorage,
+  ref,
+  uploadBytes,
 } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-storage.js";
 
 const secFirebaseConfig = {
-    apiKey: "AIzaSyBepZFDy6U3GQaJyTib-uItZOqLuW_Cv_o",
-    authDomain: "blooketleaderboardserver.firebaseapp.com",
-    databaseURL: "https://blooketleaderboardserver-default-rtdb.firebaseio.com/",
-    projectId: "blooketleaderboardserver",
-    storageBucket: "blooketleaderboardserver.appspot.com",
-    messagingSenderId: "145605729271",
-    appId: "1:145605729271:web:f02924e11bfb1472256cf2"
+  apiKey: "AIzaSyBepZFDy6U3GQaJyTib-uItZOqLuW_Cv_o",
+  authDomain: "blooketleaderboardserver.firebaseapp.com",
+  databaseURL: "https://blooketleaderboardserver-default-rtdb.firebaseio.com/",
+  projectId: "blooketleaderboardserver",
+  storageBucket: "blooketleaderboardserver.appspot.com",
+  messagingSenderId: "145605729271",
+  appId: "1:145605729271:web:f02924e11bfb1472256cf2"
 };
 const app = initializeApp(secFirebaseConfig);
 const storage = getStorage(app);
 
 let storageRef;
 async function uploadImage() {
-    OI = [JSON.parse(document.getElementById("statsInput").value)];
-    console.log(OI[0]);
-    let temp = document.getElementById("displayNameInput").value;
-    let displayName = profanityCleaner.clean(temp);
-    let leaderboard = document.getElementById("lbSubmit").value;
-    const fileInput = document.getElementById("fileInput");
-    const file = fileInput.files[0];
-    const NUMBER = namesArray.indexOf(displayName);
-    if (OI[0]._id == idArray[NUMBER]) {
-        if (leaderboard == null) {
-            alert("You need to pick a category for your run!");
-        } else {
-            if (displayName == null) {
-                alert('Please enter your display name in the "Display Name" box.');
-            } else {
-                console.log("wowie!!");
-                if (file) {
-                    alert("Stay on this page until you are instructed to do so. Press ok to confirm.");
-                    storageRef = ref(storage, `${displayName}/${leaderboard}`); //I assume something to get the file and set its name
-                    await uploadBytes(storageRef, file); //Uploads File
-                    console.log("In the database!");
-                    alert("You may now leave the webpage!");
-                }
-            }
-        }
+  OI = [JSON.parse(document.getElementById("statsInput").value)];
+  console.log(OI[0]);
+  let temp = document.getElementById("displayNameInput").value;
+  let displayName = profanityCleaner.clean(temp);
+  let leaderboard = document.getElementById("lbSubmit").value;
+  const fileInput = document.getElementById("fileInput");
+  const file = fileInput.files[0];
+  const NUMBER = namesArray.indexOf(displayName);
+
+  let today = new Date();
+  let dd = String(today.getDate()).padStart(2, '0');
+  let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+  let yyyy = today.getFullYear();
+  today = `${yyyy}-${mm}-${dd}`;
+
+  if (OI[0]._id == idArray[NUMBER]) {
+    if (leaderboard == null) {
+      alert("You need to pick a category for your run!");
     } else {
-        alert("It looks like no one in the database has your ID. Try using a different account for your stats or repaste your account stats!");
+      if (displayName == null) {
+        alert('Please enter your display name in the "Display Name" box.');
+      } else {
+        console.log("wowie!!");
+        if (file) {
+          alert("Stay on this page until you are instructed to do so. Press ok to confirm.");
+
+          storageRef = ref(storage, `${displayName}/${leaderboard}`); //I assume something to get the file and set its name
+          await uploadBytes(storageRef, file); //Uploads File
+
+          const runCount = getRunCount();
+          firebase.database().ref("/Runs").child(`run_${runCount + 1}`).update({
+            date: today,
+            runType: leaderboard,
+            score: null,
+            user: displayName,
+            verified: false
+          });
+
+          console.log("In the database!");
+          alert("You may now leave the webpage!");
+        }
+      }
     }
+  } else {
+    alert("It looks like no one in the database has your ID. Try using a different account for your stats or repaste your account stats!");
+  }
 }
 const submitButton = document.getElementById("videoSubmit");
 submitButton.addEventListener('click', uploadImage);
 
 function getData() {
-    firebase.database().ref("/Users").on('value', function (snapshot) {
-        let a = 0;
-        snapshot.forEach(function (childSnapshot) {
-            namesArray[a] = childSnapshot.val().name;
-            idArray[a] = childSnapshot.val().userID;
-            a++;
-        });
-        console.log(namesArray[0]);
-        console.log(idArray[0]);
+  firebase.database().ref("/Users").on('value', function (snapshot) {
+    let a = 0;
+    snapshot.forEach(function (childSnapshot) {
+      namesArray[a] = childSnapshot.val().name;
+      idArray[a] = childSnapshot.val().userID;
+      a++;
     });
+    console.log(namesArray[0]);
+    console.log(idArray[0]);
+  });
 }
 getData();
+
+function getRunCount() {
+  firebase.database().ref("/Runs").on('value', function (snapshot) {
+    let count = 0;
+    snapshot.forEach(function (run) {
+      count++;
+    });
+    return count;
+  });
+}
